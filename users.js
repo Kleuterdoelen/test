2f1811f434cebb4484cd17c65bb4ea5ead7dfbbc
Kleuterdoelen <marie.vdberghe1@gmail.com>
2026-03-05 10:25:03 +0100

    Add files via upload

---
+++ users.js
@@ -0,0 +1,6 @@
+// users.js
+const users = [
+    { email: "directie@school1.be", password: "1234", role: "directie" },
+    { email: "leerkracht1@school1.be", password: "abcd", role: "leerkracht" },
+    { email: "leerkracht2@school1.be", password: "efgh", role: "leerkracht" }
+  ];
